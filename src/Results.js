c88cc6fa9d06e629b6834c5ca484dda2f5025369
Eucharia2124 <obiageristoehr2124@gmail.com>
2026-05-22 15:13:18 +0200

    updated

--- src/Results.js
+++ src/Results.js
@@ -1,26 +1,21 @@
 import React from "react";
 import Meaning from "./Meaning";
 
-
 export default function Results(props) {
-    console.log(props.results);
-    if (!props.results) {
-        return null;
-    } 
-    const meanings = props.results.meanings;
-
+  if (props.results) {
     return (
-        <div className="Results">
-            <h2>{props.results.word || ""}</h2>
-            {Array.isArray(meanings) && meanings.length > 0
-                ? meanings.map(function (meaning, index) {
-                      return (
-                          <div key={index}>
-                              <Meaning meaning={meaning} />
-                          </div>
-                      );
-                  })
-                : null}
-        </div>
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
     );
+  } else {
+    return null;
+  }
 }

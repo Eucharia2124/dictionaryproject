ffc544b0a61c5a813931cf77734f2a5cb8102613
Eucharia2124 <obiageristoehr2124@gmail.com>
2026-05-31 22:15:22 +0200

    photos file created using shecodes api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword || "");
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handleSheCodeResponse(response) {
+     
+     setPhotos(response.data.photos);
   }
 
   function search(keyword) {
@@ -17,6 +24,9 @@ export default function Dictionary(props) {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
 
+    let apiSheCodeKey = "35f4a664892ct08f69ca41oe92eb5099"
+    let apiSheCodeUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiSheCodeKey}`;
+    axios.get(apiSheCodeUrl).then(handleSheCodeResponse);
   }
 
 
@@ -28,6 +38,7 @@ export default function Dictionary(props) {
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
   function load() {
     setLoaded(true);
     search(keyword);
@@ -46,6 +57,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
@@ -53,3 +65,4 @@ export default function Dictionary(props) {
     return "Loading...";
   }
 }
+
